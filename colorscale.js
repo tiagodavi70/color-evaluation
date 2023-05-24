f2a6b28a43b9a7fbe203385d5735e9c52c3192aa
Tiago Araújo <tiagodavi70@gmail.com>
2023-05-25 00:38:24 +0100

    first commit

---
+++ colorscale.js
@@ -0,0 +1,33 @@
+import {html, css, svg, LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+
+export class CategoricalColors extends LitElement {
+  static styles = css`
+    p { color: blue };
+  `;
+
+  static properties = {
+    width: {type: Number},
+    colors: {type: Object}
+  };
+
+  constructor() {
+    super();
+    this.colors = ["red", "blue", "yellow", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue"];
+    this.width = 500;
+    this.height = this.width / this.colors.length;
+    this.padding = this.height / 10;
+  }
+
+  render() {
+    let colorSize = ((this.width - this.padding) / this.colors.length);
+    return html`<div style="display: flex; margin: 1px;">
+      ${this.colors.map((c,i) =>
+       html`<div style="background-color:${c}; border: 1px solid black; width:${colorSize - this.padding}; height:${this.height - 2*this.padding}"></div>`)
+    }</div>`
+	// html`<svg width="${this.width}" height="${this.height}">
+    //   ${this.colors.map((c,i) =>
+    //    svg`<rect x=${(i * colorSize) + this.padding} y="${this.padding}" width="${colorSize - this.padding}" height="${this.height - 2*this.padding}" style="fill:${c}"></rect>`)
+    // }</svg>`;
+  }
+}
+customElements.define('categorical-colors', CategoricalColors);
